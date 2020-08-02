4abfe5e1f59dd29e433c43f2c697aa0d9220bd8d
s6hay85 <69112535+s6hay85@users.noreply.github.com>
2020-08-02 22:12:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@ SH_PATH=$(cd "$(dirname "$0")";pwd)
 cd ${SH_PATH}
 
 create_mainfest_file(){
-    echo "进行配置。。。"
-    read -p "请输入你的应用名称：" IBM_APP_NAME
-    echo "应用名称：${IBM_APP_NAME}"
-    read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
+    echo "Configuring"
+    read -p "Please enter your app name:" IBM_APP_NAME
+    echo "App name:${IBM_APP_NAME}"
+    read -p "Please enter your memory size(default: 256MB):" IBM_MEM_SIZE
     if [ -z "${IBM_MEM_SIZE}" ];then
     IBM_MEM_SIZE=256
     fi
-    echo "内存大小：${IBM_MEM_SIZE}"
+    echo "Memory size:${IBM_MEM_SIZE}"
     UUID=$(cat /proc/sys/kernel/random/uuid)
-    echo "生成随机UUID：${UUID}"
+    echo "Generating random UUID:${UUID}"
     WSPATH=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
-    echo "生成随机WebSocket路径：${WSPATH}"
+    echo "Generating random WebSocket path:${WSPATH}"
     
     cat >  ${SH_PATH}/IBMYes/v2ray-cloudfoundry/manifest.yml  << EOF
     applications:
@@ -54,12 +54,12 @@ EOF
         ]
     }
 EOF
-    echo "配置完成。"
+    echo "Configuration done."
 }
 
 clone_repo(){
-    echo "进行初始化。。。"
-    git clone https://github.com/CCChieh/IBMYes
+    echo "Initializing"
+    git clone https://github.com/s6hay85/IBM
     cd IBMYes
     git submodule update --init --recursive
     cd v2ray-cloudfoundry/v2ray
@@ -71,16 +71,16 @@ clone_repo(){
     TMP_FILE="$(mktemp)"
     if ! curl -s -o "$TMP_FILE" 'https://api.github.com/repos/v2fly/v2ray-core/releases/latest'; then
         rm "$TMP_FILE"
-        echo 'error: 获取最新V2Ray版本号失败。请重试'
+        echo 'error: Failed to get the latest version number of V2Ray.'
         exit 1
     fi
     RELEASE_LATEST="$(sed 'y/,/\n/' "$TMP_FILE" | grep 'tag_name' | awk -F '"' '{print $4}')"
     rm "$TMP_FILE"
-    echo "当前最新V2Ray版本为$RELEASE_LATEST"
+    echo "Latest V2Ray version: $RELEASE_LATEST"
     # Download latest release
     DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/$RELEASE_LATEST/v2ray-linux-64.zip"
     if ! curl -L -H 'Cache-Control: no-cache' -o "latest-v2ray.zip" "$DOWNLOAD_LINK"; then
-        echo 'error: 下载V2Ray失败，请重试'
+        echo 'error: Failed to download V2Ray.'
         return 1
     fi
     unzip latest-v2ray.zip v2ray v2ctl geoip.dat geosite.dat
@@ -88,18 +88,18 @@ clone_repo(){
     
     chmod 0755 ./*
     cd ${SH_PATH}/IBMYes/v2ray-cloudfoundry
-    echo "初始化完成。"
+    echo "Initialization done"
 }
 
 install(){
-    echo "进行安装。。。"
+    echo "Installing:"
     cd ${SH_PATH}/IBMYes/v2ray-cloudfoundry
     ibmcloud target --cf
     ibmcloud cf install
     ibmcloud cf push
-    echo "安装完成。"
-    echo "生成的随机 UUID：${UUID}"
-    echo "生成的随机 WebSocket路径：${WSPATH}"
+    echo "Installation done"
+    echo "Random UUID generated:${UUID}"
+    echo "Random WebSocket path generated:${WSPATH}"
     VMESSCODE=$(base64 -w 0 << EOF
     {
       "v": "2",
@@ -116,7 +116,7 @@ install(){
     }
 EOF
     )
-	echo "配置链接："
+	echo "Configuration:"
     echo vmess://${VMESSCODE}
 
 }
